e625f92764e63f9a620ce08f46e0f0ecfe1a5cf5
RamZouzBEN <43472649+RamZouzBEN@users.noreply.github.com>
2025-09-11 16:45:55 +0200

    Update backend.tf

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-TST" # le bucket créé plus haut
+    bucket         = "terraformstatetst" # le bucket créé plus haut
     key            = "terraform-apply/infra/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-locks-TST"
