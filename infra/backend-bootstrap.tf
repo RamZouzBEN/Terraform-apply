182b9d594905163a00754c62fce6913fe1d23a60
RamZouzBEN <43472649+RamZouzBEN@users.noreply.github.com>
2025-09-11 16:32:30 +0200

    Create backend-bootstrap.tf

---
+++ infra/backend-bootstrap.tf
@@ -0,0 +1,37 @@
+resource "aws_s3_bucket" "tf_state" {
+  bucket = "terraform-state-TST"   # unique globalement
+  acl    = "private"
+
+  versioning {
+    enabled = true
+  }
+
+  server_side_encryption_configuration {
+    rule {
+      apply_server_side_encryption_by_default {
+        sse_algorithm = "AES256"
+      }
+    }
+  }
+
+  tags = {
+    Name = "terraform-state-TST"
+    Environment = TST
+  }
+}
+
+resource "aws_dynamodb_table" "tf_locks" {
+  name           = "terraform-locks-TST"
+  billing_mode   = "PAY_PER_REQUEST"
+  hash_key       = "LockID"
+
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
+  tags = {
+    Name = "terraform-locks-TST"
+    Environment = TST
+  }
+}
