2fca6756c5978a1956a4dc8d42045875369023a2
RamZouzBEN <43472649+RamZouzBEN@users.noreply.github.com>
2025-09-11 16:33:56 +0200

    Create backend.tf

---
+++ infra/backend.tf
@@ -0,0 +1,9 @@
+terraform {
+  backend "s3" {
+    bucket         = "terraform-state-prod"   # le bucket créé plus haut
+    key            = "terraform-apply/infra/terraform.tfstate"
+    region         = "eu-west-1"
+    dynamodb_table = "terraform-locks-prod"
+    encrypt        = true
+  }
+}
