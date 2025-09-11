989bd7f4fed161ca27fcbfe5d41dfdd5738d49c8
RamZouzBEN <43472649+RamZouzBEN@users.noreply.github.com>
2025-09-11 15:05:41 +0000

    chore(fmt): terraform auto-format

--- infra/backend-bootstrap.tf
+++ infra/backend-bootstrap.tf
@@ -9,7 +9,7 @@ provider "aws" {
 
 # 1) Seul le bucket (sans versioning/SSE/PAB ici)
 resource "aws_s3_bucket" "tf_state" {
-  bucket = "terraformstatetst"  # mets EXACTEMENT le nom que tu utilises
+  bucket = "terraformstatetst" # mets EXACTEMENT le nom que tu utilises
 
   tags = {
     Name        = "terraform-state-${var.env}"
@@ -50,7 +50,7 @@ resource "aws_s3_bucket_public_access_block" "tf_state" {
 
 # 5) Table DynamoDB pour les locks
 resource "aws_dynamodb_table" "tf_locks" {
-  name         = "terraform-locks-TST"   # EXACTEMENT le nom que tu utilises
+  name         = "terraform-locks-TST" # EXACTEMENT le nom que tu utilises
   billing_mode = "PAY_PER_REQUEST"
   hash_key     = "LockID"
 
