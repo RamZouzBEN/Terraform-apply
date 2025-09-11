7cb21f302993fd5933451fdeef78f27fe28ee4aa
RamZouzBEN <43472649+RamZouzBEN@users.noreply.github.com>
2025-09-11 14:34:09 +0000

    chore(fmt): terraform auto-format

--- infra/backend.tf
+++ infra/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket         = "terraform-state-prod"   # le bucket créé plus haut
+    bucket         = "terraform-state-prod" # le bucket créé plus haut
     key            = "terraform-apply/infra/terraform.tfstate"
     region         = "eu-west-1"
     dynamodb_table = "terraform-locks-prod"
