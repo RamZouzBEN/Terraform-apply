f48eb2e2800303e23960aa4366aee6b7589e4b2e
RamZouzBEN <43472649+RamZouzBEN@users.noreply.github.com>
2025-09-11 16:45:31 +0200

    Update backend-bootstrap.tf

--- infra/backend-bootstrap.tf
+++ infra/backend-bootstrap.tf
@@ -1,5 +1,5 @@
 resource "aws_s3_bucket" "tf_state" {
-  bucket = "terraform-state-TST" # unique globalement
+  bucket = "terraformstatetst" # unique globalement
   acl    = "private"
 
   versioning {
